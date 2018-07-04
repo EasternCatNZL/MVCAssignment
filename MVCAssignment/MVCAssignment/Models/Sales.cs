2b53483eb9ba0477194e5dc0edf7df59a687d643
EasternCatNZL <brianpwwong@gmail.com>
2018-07-04 16:34:03 +1200

    Made and filled out model classes

---
+++ MVCAssignment/MVCAssignment/Models/Sales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment.Models
+{
+    public class Sales
+    {
+        public int ProductID { get; set; }
+        public int CustomerID { get; set; }
+        public int StoreID { get; set; }
+        public DateTime DateSold { get; set; }
+    }
+}
