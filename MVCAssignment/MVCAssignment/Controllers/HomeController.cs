600ff193b86ff17ba9e2132b36f5ee4ec6e6ad04
EasternCatNZL <brianpwwong@gmail.com>
2018-07-04 15:54:46 +1200

    Added view files

--- MVCAssignment/MVCAssignment/Controllers/HomeController.cs
+++ MVCAssignment/MVCAssignment/Controllers/HomeController.cs
@@ -6,24 +6,35 @@ using System.Web.Mvc;
 
 namespace MVCAssignment.Controllers
 {
+    //calls view cshtmls
     public class HomeController : Controller
     {
+        //base home page view
         public ActionResult Index()
         {
             return View();
         }
 
+        //about view
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application about page.";
 
             return View();
         }
 
+        //contact view
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        //Product page view
+        public ActionResult Product()
+        {
+            ViewBag.Message = "Our product lineup.";
             return View();
         }
     }
