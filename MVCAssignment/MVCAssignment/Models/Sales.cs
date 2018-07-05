d5d60492a37663da7d466abe8cf44965965c956a
EasternCatNZL <brianpwwong@gmail.com>
2018-07-05 16:40:55 +1200

    Trying this again, following multiple table tutorial thingy this time

--- MVCAssignment/MVCAssignment/Models/Sales.cs
+++ MVCAssignment/MVCAssignment/Models/Sales.cs
@@ -9,15 +9,18 @@ namespace MVCAssignment.Models
     //Model details
     public class Sales
     {
-        public int ProductID { get; set; }
-        public int CustomerID { get; set; }
-        public int StoreID { get; set; }
+        //public int ProductID { get; set; }
+        //public int CustomerID { get; set; }
+        //public int StoreID { get; set; }
         public DateTime DateSold { get; set; }
     }
 
     //database context
     public class SalesDBContext : DbContext
     {
-        public DbSet<Sales> SalesDB { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<CustomerDetails> Customer { get; set; }
+        public DbSet<Store> Store { get; set; }
+        public DbSet<Sales> SaleDate { get; set; }
     }
 }
